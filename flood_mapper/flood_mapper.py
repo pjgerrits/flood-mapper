1c3141e1f82c04f3c39a9d08d83310adb8262479
cate <catherine.seale@ukho.gov.uk>
2022-02-27 19:51:49 +0000

    add docstrings

--- flood_mapper/flood_mapper.py
+++ flood_mapper/flood_mapper.py
@@ -3,7 +3,20 @@ import ee
 ee.Initialize()
 
 
-def retrieve_image_collection(region, start_date, end_date, polarization="VH", pass_direction="ASCENDING"):
+def retrieve_image_collection(search_region, start_date, end_date, polarization="VH", pass_direction="ASCENDING"):
+    """
+
+    Inputs:
+        search_region (ee.Geometry.Polygon): Geographic extent of image search.
+        start_date (str): Date in format yyyy-mm-dd, e.g., '2020-10-01'.
+        end_date (str): Date in format yyyy-mm-dd, e.g., '2020-10-01'.
+        polarization (str): Synthetic aperture radar polarization mode, e.g., 'VH' or 'VV'. VH is mostly is the preferred
+            polarization for flood mapping.
+        pass_direction (str): Synthetic aperture radar pass direction, either 'ASCENDING' or 'DESCENDING'.
+
+    Returns:
+        collection (ee.ImageCollection): Sentinel-1 images matching the search criteria.
+    """
 
     collection = ee.ImageCollection('COPERNICUS/S1_GRD')\
         .filter(ee.Filter.eq('instrumentMode', 'IW'))\
@@ -11,42 +24,79 @@ def retrieve_image_collection(region, start_date, end_date, polarization="VH", p
         .filter(ee.Filter.eq('orbitProperties_pass', pass_direction))\
         .filter(ee.Filter.eq('resolution_meters', 10))\
         .filterDate(start_date, end_date)\
-        .filterBounds(region)\
+        .filterBounds(search_region)\
         .select(polarization)
 
     return collection
 
 
 def smooth(image, smoothing_radius=50):
-    # Apply reduce the radar speckle by smoothing
+    """
+    Reduce the radar speckle by smoothing.
+
+    Inputs:
+        image (ee.Image): Input image.
+        smoothing_radius (int): The radius of the kernel to use for focal mean smoothing.
+
+    Returns:
+        smoothed_image (ee.Image): The resulting image after smoothing is applied.
+    """
+
     smoothed_image = image.focal_mean(smoothing_radius, 'circle', 'meters')
+
     return smoothed_image
 
 
 def mask_permanent_water(image):
+    """
+    Query the JRC Global Surface Water Mapping Layers, v1.3, to determine where perennial water bodies
+    (water > 10 months/yr), and mask these areas.
 
-    surface_water = ee.Image('JRC/GSW1_0/GlobalSurfaceWater').select('seasonality')
+    Inputs:
+        image (ee.Image): Input image.
+
+    Returns:
+        masked_image (ee.Image): The resulting image after surface water masking is applied.
+    """
+
+    surface_water = ee.Image('JRC/GSW1_3/GlobalSurfaceWater').select('seasonality')
     surface_water_mask = surface_water.gte(10).updateMask(surface_water.gte(10))
 
     # Flooded layer where perennial water bodies(water > 10 mo / yr) is assigned a 0 value
-    flooded_mask = image.where(surface_water_mask, 0)
+    where_surface_water = image.where(surface_water_mask, 0)
 
-    # Final flooded area
-    flooded_area = image.updateMask(flooded_mask)
+    masked_image = image.updateMask(where_surface_water)
 
-    return flooded_area
+    return masked_image
 
 
 def reduce_noise(image):
-    # Compute connectivity of pixels to eliminate those connected to 8 or fewer neighbours
-    # This operation reduces noise of the flood extent product
-    connections = image.connectedPixelCount()
-    flooded_area_reduced_noise = image.updateMask(connections.gte(8))
+    """
+    Compute connectivity of pixels to eliminate those connected to 8 or fewer neighbours. This operation reduces noise
+    of the flood extent product.
 
-    return flooded_area_reduced_noise
+    Inputs:
+        image (ee.Image): A binary image.
+
+    Returns:
+        reduced_noise_image (ee.Image): The resulting image after noise reduction is applied.
+    """
+
+    connections = image.connectedPixelCount()
+    reduced_noise_image = image.updateMask(connections.gte(8))
+
+    return reduced_noise_image
 
 
 def mask_slopes(image):
+    """
+    Mask out areas with more than 5 percent slope using a Digital Elevation Model.
+
+    Inputs:
+        image (ee.Image): Input image.
+    Returns:
+         slopes_masked (ee.Image): The resulting image after slope masking is applied.
+    """
 
     dem = ee.Image('WWF/HydroSHEDS/03VFDEM')
     terrain = ee.Algorithms.Terrain(dem)
@@ -56,25 +106,28 @@ def mask_slopes(image):
     return slopes_masked
 
 
-# def geometry_checker(geometry):
-#     """
-#     Checks to see if user AOI is a valid Earth Engine geometry
-#     :return:
-#     """
-#
-#     if # geomety is valid
-#         return True
-#
-#     else:
-#         print('Input geometry is not a valid geomstry.')
-#         return False
+def derive_flood_extents(aoi, before_start_date, before_end_date, after_start_date, after_end_date,
+                         difference_threshold=1.25):
+    """
+    Set start and end dates of a period BEFORE and AFTER a flood. These periods need to be long enough for Sentinel-1 to
+    acquire an image.
 
+    Inputs:
+        aoi (ee.Geometry.Polygon): Geographic extent of analysis area.
+        before_start_date (str): Date in format yyyy-mm-dd, e.g., '2020-10-01'.
+        before_end_date (str): Date in format yyyy-mm-dd, e.g., '2020-10-01'.
+        after_start_date (str): Date in format yyyy-mm-dd, e.g., '2020-10-01'.
+        after_end_date (str): Date in format yyyy-mm-dd, e.g., '2020-10-01'.
+        difference_threshold (float): Threshold to be applied on the differenced image (after flood - before flood). It
+            has been chosen by trial and error. In case your flood extent result shows many false-positive or negative
+            signals, consider changing it.
 
-def flood_mapper_func(aoi, before_start_date, before_end_date, after_start_date, after_end_date, difference_threshold=1.25):
+    Returns:
+        flood_vectors (ee.FeatureCollection): Detected flood extents as vector geometries.
+        flood_rasters (ee.Image): Detected flood extents as a binary raster.
+        flooded_image (ee.Image): The 'after' Sentinel-1 image containing view of the flood waters.
+    """
 
-    # TODO add geometry checker function
-
-    # Select images by predefined dates
     before_flood_img_col = retrieve_image_collection(aoi, before_start_date, before_end_date)
     after_flood_img_col = retrieve_image_collection(aoi, after_start_date, after_end_date)
 
@@ -102,30 +155,6 @@ def flood_mapper_func(aoi, before_start_date, before_end_date, after_start_date,
                                                   bestEffort=True,
                                                   tileScale=2)
 
-    s1_imagery = after_filtered
+    flooded_image = after_filtered
 
-    return flood_vectors, flood_rasters, s1_imagery
-
-
-if __name__ == "__main__":
-
-    before_start = '2020-10-01'
-    before_end = '2020-10-15'
-
-    after_start = '2020-11-04'
-    after_end = '2020-11-15'
-
-    region = ee.Geometry.Polygon([[[-85.93, 16.08],
-                                [-85.93, 15.69],
-                                [-85.40, 15.69],
-                                [-85.40, 16.08]]])
-
-    detected_flood_vector, detected_flood_raster, imagery = flood_mapper_func(region,
-                                                                         before_start,
-                                                                         before_end,
-                                                                         after_start,
-                                                                         after_end)
-
-    # print(detected_flood_vector)
-    # print(detected_flood_raster.getInfo())
-    # print(imagery)
+    return flood_vectors, flood_rasters, flooded_image
